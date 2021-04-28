e08a176ea90fd35a55fdb0545c15650976e77783
slowe <gravatar@strudel.org.uk>
2021-04-28 13:51:27 +0100

    Add 2020 wards

--- data/README.md
+++ data/README.md
@@ -2,10 +2,21 @@
 
 The following sub-directories contain individual geojson blobs for:
 
-* __[Local Authority Districts 2020](LAD20CD/)__ e.g. [LAD20CD/E08000035.geojsonl](LAD20CD/E08000035.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/local-authority-districts-may-2020-boundaries-uk-bgc) (OGL)
+* __[Local Authority Districts 2020](LAD20CD/)__ e.g. [LAD20CD/E08000035.geojsonl](LAD20CD/E08000035.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/local-authority-districts-december-2020-uk-bgc) (OGL)
 * __[Local Authority Districts 2019](LAD19CD/)__ e.g. [LAD19CD/E08000035.geojsonl](LAD19CD/E08000035.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/local-authority-districts-december-2019-boundaries-uk-bgc) (OGL)
 * __[Wards 2019](WD19CD/)__ e.g. [WD19CD/E05011403.geojsonl](WD19CD/E05011403.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/wards-december-2019-boundaries-ew-bgc) (OGL)
+* __[Wards 2020](WD20CD/)__ e.g. [WD20CD/E05011403.geojsonl](WD20CD/E05011403.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/wards-december-2020-uk-bgc-v2) (OGL)
 * __[Parliamentary constituencies 2017](PCON17CD/)__ e.g. [PCON17CD/E14000777.geojsonl](PCON17CD/E14000777.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/wards-december-2019-boundaries-ew-bgc) (OGL)
 * __[MSOAs 2011](MSOA11CD/)__ e.g. [MSOA11CD/E02006875.geojsonl](MSOA11CD/E02006875.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-ew-bgc) (OGL)
 * __[LSOAs 2011](LSOA11CD/)__ e.g. [LSOA11CD/E01033011.geojsonl](LSOA11CD/E01033011.geojsonl) - source: [ONS](https://geoportal.statistics.gov.uk/datasets/lower-layer-super-output-areas-december-2011-boundaries-ew-bgc) (OGL)
 * __[LSOAs 2001](LSOA01CD/)__ e.g. [LSOA01CD/E01011365.geojsonl](LSOA01CD/E01011365.geojsonl) - source: ONS (OGL)
+
+The data is downloaded from ONS GeoPortal as a `.geojson` file and stored in e.g. `WD20CD/temp/`. We then run:
+
+```
+perl process.pl <FILE> <CODE>
+
+perl process.pl WD20CD/temp/Wards_\(December_2020\)_UK_BGC_V2.geojson WD20CD
+```
+
+where `<FILE>` is the location of the `.geojson` file and `<CODE>` is the ONS geocode e.g. `WD20CD`.
--- data/process.pl
+++ data/process.pl
@@ -1,37 +1,95 @@
 #!/usr/bin/perl
 
+use JSON::XS;
+use utf8;
+
 $file = $ARGV[0];
-
 $dir = $ARGV[1];
+$precision = 5;
 
 
-if($file !~ /geojsonl/){
-
-	print "Input file must be a geojsonl file.\n";
+if(!-e $file || $file !~ /\.geojson$/){
+	print "Input file must be a geojson file.\n";
 	exit;
 }
 
+if(!$dir){
+	print "Please provide an ONS code as the second argument.";
+	exit;
+}
+
+# Create directories if they don't exist
 if(!-d $dir){
-	print "Directory $dir doesn't exist. Please make it first.\n";
-	exit;
+	`mkdir $dir`;
+	if(!-d $dir."/temp/"){
+		`mkdir $dir/temp/`;
+	}
 }
 
-
+$out = "";
 open(FILE,$file);
-while (my $line = <FILE>) {
-    chomp $line;
-	if($line =~ /\"$dir\":\s*"([^\"]*)"/i){
-		$id = $1;
-		$line =~ s/([0-9]), ([0-9\-\+])/$1,$2/g;
-		$line =~ s/ \], \[ /\]\,\[/g;
-		$line =~ s/\"([,\:]) \"/\"$1\"/g;
-		$line =~ s/\[ \[ ?/\[\[/g;
-		$line =~ s/\] \] ?/\]\]/g;
-		$line =~ s/([\{\[]) \"/$1\"/g;
-		$line =~ s/\": ([\{\[])/\":$1/g;
-		open(BIT,">",$dir."/".$id.".geojsonl");
-		print BIT $line;
-		close(BIT);
+while(<FILE>){
+	$line = $_;
+	if($line =~ /"type":\s+"Feature"/){
+		$line =~ s/\,$//g;
+		$json = JSON::XS->new->decode($line);
+		$jsonl = "";
+		$cc = "";
+		if($json->{'geometry'}{'coordinates'}){
+			@coord = @{$json->{'geometry'}{'coordinates'}};
+			$ni = @coord;
+			$cc .= "[";
+			for($i = 0; $i < $ni; $i++){
+				$nj = @{$coord[$i]};
+				$cc .= ($i > 0 ? ",":"")."[";
+				for($j = 0; $j < $nj; $j++){
+					$nk = @{$coord[$i][$j]};
+					$cc .= ($j > 0 ? ",":"")."[";
+					if($json->{'geometry'}{'type'} eq "MultiPolygon"){
+						for($k = 0; $k < $nk; $k++){
+							$cc .= ($k > 0 ? ",":"")."[";
+							$cc .= sprintf('%.'.$precision.'f', $coord[$i][$j][$k][0]).",".sprintf('%.'.$precision.'f', $coord[$i][$j][$k][1]);
+							$cc .= "]";
+						}
+					}else{
+						$cc .= sprintf('%.'.$precision.'f', $coord[$i][$j][0]).",".sprintf('%.'.$precision.'f', $coord[$i][$j][1]);
+					}
+					$cc .= "]";
+				}
+				$cc .= "]";
+			}
+			$cc .= "]";
+		}
+		foreach $prop (keys(%{$json->{'properties'}})){
+			if($prop !~ /^[A-Za-z]+[0-9]{2}(CD|NM)/){
+				delete $json->{'properties'}{$prop};
+			}
+			if($json->{'properties'}{$prop} eq "" || $json->{'properties'}{$prop} eq " "){
+				delete $json->{'properties'}{$prop};
+			}
+		}
+		$jsonl .= "{ \"type\": \"Feature\", \"properties\": { ";
+		$p = "";
+		foreach $prop (sort(keys(%{$json->{'properties'}}))){
+			$p .= ($p ? ", ":"")."\"$prop\": \"$json->{'properties'}{$prop}\"";
+		}
+		$jsonl .= $p;
+		$jsonl .= " }, \"geometry\": { \"type\": \"$json->{'geometry'}{'type'}\", \"coordinates\": $cc }";
+		$jsonl .= "}\n";
+
+		if($json->{'properties'}{$dir}){
+			$id = $json->{'properties'}{$dir};
+			open(BIT,">",$dir."/".$id.".geojsonl");
+			print BIT $jsonl;
+			close(BIT);
+		}
+		$out .= $jsonl;
 	}
 }
 close(FILE);
+
+open(JSON,">","$code/temp/$code.geojsonl.json");
+print JSON $out;
+close(JSON);
+
+print "Done.\n";
